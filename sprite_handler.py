1c81b020e6e286a9ae6a0fcf77a60ded4e796e6f
aesom-e <114953191+aesom-e@users.noreply.github.com>
2025-06-02 12:55:52 -0400

    Added support for animations for the sprite handler

--- sprite_handler.py
+++ sprite_handler.py
@@ -18,15 +18,15 @@ _sprites = []
 _drawn_this_frame = []
 
 def register_sprite(sprite_rectangle: tuple[float, float, float, float, str | None],
-                    image_path: str,
+                    image_path: str | list[str],
                     transparent: bool,
                     on_collision: Callable[[None], None]) -> int:
     """Registers a new sprite with the sprite handler
     
     Args:
         sprite_rectangle (tuple[float, float, float, float, str | None]): The sprite's rectangle
-        image_path (str): The path to the sprite's image
-        transparent (bool): Whether the sprite's image contains transparency
+        image_path (str | list[str]): The path to the sprite's image or list of images in its animation
+        transparent (bool): Whether the sprite's image contains transparency. If it is an animation, it must be consistant
         on_collision (Callable[[None], None]): The function to call on collision with the player
         
     Returns:
@@ -36,19 +36,31 @@ def register_sprite(sprite_rectangle: tuple[float, float, float, float, str | No
     true_rectangle = rectangle.get_rectangle(sprite_rectangle)
 
     # Preconstruct the sprite's image to save time later
-    if transparent:
-        sprite_image = pygame.transform.scale(pygame.image.load(image_path).convert_alpha(),
-                                              (true_rectangle[2], true_rectangle[3]))
+    if isinstance(image_path, list):
+        if transparent:
+            sprite_image = [pygame.transform.scale(pygame.image.load(path).convert_alpha(),
+                                                  (true_rectangle[2], true_rectangle[3]))
+                            for path in image_path]
+        else:
+            sprite_image = [pygame.transform.scale(pygame.image.load(path).convert(),
+                                                  (true_rectangle[2], true_rectangle[3]))
+                            for path in image_path]
+        frame_number = 0
     else:
-        sprite_image = pygame.transform.scale(pygame.image.load(image_path).convert(),
-                                              (true_rectangle[2], true_rectangle[3]))
+        frame_number = None
+        if transparent:
+            sprite_image = pygame.transform.scale(pygame.image.load(image_path).convert_alpha(),
+                                                 (true_rectangle[2], true_rectangle[3]))
+        else:
+            sprite_image = pygame.transform.scale(pygame.image.load(image_path).convert(),
+                                                 (true_rectangle[2], true_rectangle[3]))
 
-    _sprites.append([true_rectangle, image_path, sprite_image, on_collision, False])
+    _sprites.append([true_rectangle, image_path, transparent, sprite_image, on_collision, False, frame_number])
     return len(_sprites)-1
 
 def modify_sprite(sprite_id: int,
                   sprite_rectangle: tuple[float, float, float, float, str | None] = None,
-                  image_path: str = None,
+                  image_path: str | list[str] = None,
                   transparent: bool = None,
                   on_collision: Callable[[None], None] = False) -> None:
     """Modifies a sprite based on its ID
@@ -57,8 +69,8 @@ def modify_sprite(sprite_id: int,
         sprite_id (int): The ID of the sprite to modify
         sprite_rectangle (tuple[float, float, float, float, str | None]): The new rectangle for the sprite;
                                                                           leave blank to not modify
-        image_path (str): The path of the sprite's image; leave blank to not modify
-        transparent (bool): True if the sprite's image is transparent, False if not. Needs to be set if image_path is not None
+        image_path (str | list[str]): The path of the sprite's image or list of images; leave blank to not modify
+        transparent (bool): True if the sprite's image is transparent, False if not. Needs to be set if image_path or rectangle is not None
         on_collision (Callable[[None], None]): The function to call upon player collision with the sprite; leave blank to not modify
     """
     if sprite_rectangle is not None:
@@ -66,23 +78,50 @@ def modify_sprite(sprite_id: int,
         
         # Don't waste compute time
         if image_path is None:
-            sprite_image = pygame.transform.scale(pygame.image.load(_sprites[sprite_id][1]).convert_alpha(),
-                                                  (true_rectangle[2], true_rectangle[3]))
-            _sprites[sprite_id][2] = sprite_image
+            if isinstance(image_path, list):
+                if _sprites[sprite_id][2]:
+                    sprite_image = [pygame.transform.scale(pygame.image.load(path).convert_alpha(),
+                                                          (true_rectangle[2], true_rectangle[3]))
+                                    for path in image_path]
+                else:
+                    sprite_image = [pygame.transform.scale(pygame.image.load(path).convert_alpha(),
+                                                          (true_rectangle[2], true_rectangle[3]))
+                                    for path in image_path]
+                _sprites[sprite_id][6] = 0 # Reset the frame count
+            else:
+                if _sprites[sprite_id][2]:
+                    sprite_image = pygame.transform.scale(pygame.image.load(_sprites[sprite_id][1]).convert_alpha(),
+                                                         (true_rectangle[2], true_rectangle[3]))
+                else:
+                    sprite_image = pygame.transform.scale(pygame.image.load(_sprites[sprite_id][1]).convert(),
+                                                         (true_rectangle[2], true_rectangle[3]))
+            _sprites[sprite_id][3] = sprite_image
         
         _sprites[sprite_id][0] = true_rectangle
     if image_path is not None:
         if transparent is None:
             raise ValueError(f"If image_path is set, transparent must be as well")
         _sprites[sprite_id][1] = image_path
-        if transparent:
-            _sprites[sprite_id][2] = pygame.transform.scale(pygame.image.load(image_path).convert_alpha(),
-                                                            (_sprites[sprite_id][0][2], _sprites[sprite_id][0][3]))
+        
+        if isinstance(image_path, list):
+            if transparent:
+                sprite_image = [pygame.transform.scale(pygame.image.load(path).convert_alpha(),
+                                                      (true_rectangle[2], true_rectangle[3]))
+                                for path in image_path]
+            else:
+                sprite_image = [pygame.transform.scale(pygame.image.load(path).convert(),
+                                                      (true_rectangle[2], true_rectangle[3]))
+                                for path in image_path]
+            _sprites[sprite_id][6] = 0 # Reset the frame count
         else:
-            _sprites[sprite_id][2] = pygame.transform.scale(pygame.image.load(image_path).convert(),
-                                                            (_sprites[sprite_id][0][2], _sprites[sprite_id][0][3]))
+            if transparent:
+                _sprites[sprite_id][3] = pygame.transform.scale(pygame.image.load(image_path).convert_alpha(),
+                                                                (_sprites[sprite_id][0][2], _sprites[sprite_id][0][3]))
+            else:
+                _sprites[sprite_id][3] = pygame.transform.scale(pygame.image.load(image_path).convert(),
+                                                                (_sprites[sprite_id][0][2], _sprites[sprite_id][0][3]))
     if on_collision is not False:
-        _sprites[sprite_id][3] = on_collision
+        _sprites[sprite_id][4] = on_collision
 
 def move_sprite(sprite_id: int, move: tuple[float, float]) -> None:
     """Moves a sprite
@@ -114,20 +153,29 @@ def set_sprite_status(sprite_id: int, active: bool) -> None:
         sprite_id (int): The ID of the sprite to modify
         active (bool): True to make the sprite draw, False to make it not
     """
-    _sprites[sprite_id][4] = active
+    _sprites[sprite_id][5] = active
 
 def draw_sprites() -> None:
     """Draws all the active sprites"""
-    for sprite in _sprites:
-        rectangle, image_path, surface, on_collision, active = tuple(sprite)
+    for _, sprite in enumerate(_sprites):
+        rectangle, image_path, transparent, image, on_collision, active, frame = tuple(sprite)
         if active:
-            assets.screen.blit(surface, rectangle)
+            # Check if the image is a list; then process accordingly
+            if isinstance(image, list):
+                assets.screen.blit(image[frame], rectangle)
+                
+                # Increment the frame counter
+                _sprites[6] += 1
+                if _sprites[6] >= len(image):
+                    _sprites[6] = 0
+            else:
+                assets.screen.blit(image, rectangle)
 
 def handle_collisions() -> None:
     """Handles all the collisions with the player"""
     global _drawn_this_frame
     for _, sprite in enumerate(_sprites):
-        rectangle, image_path, surface, on_collision, active = tuple(sprite)
+        rectangle, image_path, transparent, surface, on_collision, active, frame = tuple(sprite)
         if active or _ in _drawn_this_frame:
             if assets.player_rectangle.colliderect(rectangle):
                 if on_collision is not None: on_collision()
@@ -135,5 +183,5 @@ def handle_collisions() -> None:
 
 def draw_specifically(sprite_id: int) -> None:
     """Draws the specified sprite whether it's active or not"""
-    assets.screen.blit(_sprites[sprite_id][2], _sprites[sprite_id][0])
+    assets.screen.blit(_sprites[sprite_id][3], _sprites[sprite_id][0])
     _drawn_this_frame.append(sprite_id)
